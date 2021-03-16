d700a1c9c61d851c5e8794097faa077ad4f13b8f
Elle Green <02elle.green@gulllakecs.org>
2021-03-16 10:52:52 -0400

    L3.21 PT2 - Adding points when buttons are clicked

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
     }
 }
